263afb8360c4ebcaeed68b2decda013f362e7d8c
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-15 18:49:31 -0500

    implement create for each repo

--- database/index.js
+++ database/index.js
@@ -17,14 +17,22 @@ let save = (username, data) => {
 
   for(var i = 0; i < data.length; i ++) {
   //query to check if the id is already in database
-  var currentRepo = data[i];
-    Repo.create({
-      forks: currentRepo.forks_count,
-      url: currentRepo.html_url,
-      repoId: currentRepo.id,
-      username: username,
-      stargazers: currentRepo.stargazers_count
-    })
+    var currentRepo = data[i];
+    //TODO: figure out opt out of create for result array that contains something
+    // Repo.find({repoId: currentRepo.id})
+    //   .then((result) => {
+    //     if (result.length !== 0) {
+    //       console.log('repo does exists for: ', result.name);
+    //     }
+    //   })
+      Repo.create({
+        forks: currentRepo.forks_count,
+        url: currentRepo.html_url,
+        repoId: currentRepo.id,
+        username: username,
+        name: currentRepo.name,
+        stargazers: currentRepo.stargazers_count
+      })
   }
 }
 
